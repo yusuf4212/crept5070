343029a3ea99989f24c4430259a07f114777fc7e
yusuf4212 <aafandiyusuf2142@gmail.com>
2023-06-03 16:04:44 +0700

    erase old code, using local bootstrap

--- josh-table.php
+++ josh-table.php
@@ -2,18 +2,8 @@
 function josh_table() {
     global $wpdb;
     
-    // $table_settings = $wpdb->prefix.'josh_table_settings';
     $table_settings_ = $wpdb->prefix . 'dja_settings';
     
-    // $query = " SELECT menu, value FROM $table_settings ";
-    // $row = $wpdb->get_results( $query );
-    
-    // $program    = $row[0]->value;
-    // $platform   = $row[1]->value;
-    // $cs         = $row[2]->value;
-    // $type       = $row[3]->value;
-    // $bank       = $row[4]->value;
-
     $query = "SELECT data
     FROM $table_settings_
     WHERE type='program_' or type='platform_' or type='cs_' or type='bank_'";
@@ -26,8 +16,9 @@ function josh_table() {
     ?>
     
     <!-- bootstrap 5.3 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo DJA_PLUGIN_URL . 'assets/vendor/twbs/bootstrap/dist/css/bootstrap.css' ?>">
+    <script src="<?php echo DJA_PLUGIN_URL . 'assets/vendor/twbs/bootstrap/dist/js/bootstrap.js' ?>"></script>
+
     
     <!-- jQuery -->
     <!-- <script src="<?php //echo plugin_dir_url( __FILE__ ); ?>admin/assets/js/jquery.min.js"></script>
